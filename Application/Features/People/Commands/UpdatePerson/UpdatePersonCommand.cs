a281ffe37ef0a9281556f8a73325061b98751b71
JakubWaj <jw303269@student.polsl.pl>
2024-11-29 20:43:49 +0100

    UpdatePersonCommand.cs

---
+++ Application/Features/People/Commands/UpdatePerson/UpdatePersonCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstraction;
+
+namespace Application.Features.People.Commands.UpdatePerson;
+
+public class UpdatePersonCommand : ICommand<bool>
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Biography { get; set; }
+}
---
+++ Application/Features/People/Commands/UpdatePerson/UpdatePersonCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Abstraction;
+using Application.Features.Exceptions;
+using Domain.Entities;
+
+namespace Application.Features.People.Commands.UpdatePerson;
+
+public class UpdatePersonCommandHandler : ICommandHandler<UpdatePersonCommand, bool>
+{
+    private readonly IPersonRepository _personRepository;
+
+    public UpdatePersonCommandHandler(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+    
+    public async Task<bool> HandleAsync(UpdatePersonCommand command)
+    {
+        var exists = await _personRepository.ExistsAsync(command.Id);
+        if (!exists)
+        {
+            throw new NotFoundException("Person not found");
+        }
+        var person = new Person
+        {
+            Id = command.Id,
+            FirstName = command.FirstName,
+            LastName = command.LastName,
+            BirthDate = command.BirthDate,
+            Biography = command.Biography
+        };
+        await _personRepository.UpdateAsync(person);
+        return true;
+        
+    }
+}
